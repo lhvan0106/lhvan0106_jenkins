f72ae8390db2257303deb88376ac7eb702c1304d
lhvan0106-7567 <lehoangvan@gmail.com>
2020-06-11 16:43:53 +0700

    .

--- restapi/restapi.sh
+++ restapi/restapi.sh
@@ -1,2 +1,2 @@
 #nohup java -jar -Xms64m -Xmx512m -Dserver.port=100 restapi.jar > ./restapi.out &
-java -jar -Xms64m -Xmx512m -Dserver.port=100 restapi.jar
+java -jar -Xms64m -Xmx512m -Dserver.port=100 restapi-0.0.1-SNAPSHOT.jar
